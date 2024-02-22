aa7fd1ece32a1e149f33d940afe948df7bd6bfc1
Wakeru <103467521+Wakeru@users.noreply.github.com>
2024-02-21 19:24:01 -0500

    Added Folders and Background

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.musicapp
 
+import android.annotation.SuppressLint
 import android.health.connect.datatypes.units.Length
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
@@ -11,21 +12,32 @@ import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val searchButton = findViewById<ImageButton>(R.id.search)
         searchButton.setOnClickListener{
-            Toast.makeText(applicationContext,"You got me, you actually f****ing got me... how dare you...",Toast.LENGTH_LONG).show()
-            //this on click tells what the button gonna do
+            Toast.makeText(applicationContext,"You click search button",Toast.LENGTH_LONG).show() //idk what applicationContext is...
+            //this on click tells what the button is gonna do
             //if we make another activity, it launches a whole new window for the activity
             // so we need to do something in this activity...
         }
 
-        val folderButton = findViewById<ImageButton>(R.id.add)
-        folderButton.setOnClickListener{
+        val addButton = findViewById<ImageButton>(R.id.add)
+        addButton.setOnClickListener{
+            Toast.makeText(applicationContext,"You clicked Add Folder Button",Toast.LENGTH_LONG).show()
+        }
 
+        val folderButton = findViewById<ImageButton>(R.id.folder)
+        folderButton.setOnClickListener{
+            Toast.makeText(applicationContext,"You clicked MixTape",Toast.LENGTH_LONG).show()
+        }
+
+        val folderButton2 = findViewById<ImageButton>(R.id.folder2)
+        folderButton2.setOnClickListener{
+            Toast.makeText(applicationContext,"You clicked Soda Pop Music Folder",Toast.LENGTH_LONG).show()
         }
 
 
