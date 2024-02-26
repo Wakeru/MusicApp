5647728d453fbf82a3db466a6891f8d00ddd4037
vashah131 <116308234+vashah131@users.noreply.github.com>
2024-02-26 11:59:14 -0500

    Adding New Stuff

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.example.musicapp
 
-import android.health.connect.datatypes.units.Length
-import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
-import android.widget.ImageButton
+import android.widget.Button
 import android.widget.Toast
-
+import androidx.appcompat.widget.Toolbar
+import android.widget.TextView
+import android.widget.ImageView
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,21 +14,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val searchButton = findViewById<ImageButton>(R.id.search)
-        searchButton.setOnClickListener{
-            Toast.makeText(applicationContext,"You got me, you actually f****ing got me... how dare you...",Toast.LENGTH_LONG).show()
-            //this on click tells what the button gonna do
-            //if we make another activity, it launches a whole new window for the activity
-            // so we need to do something in this activity...
+        // Setup Toolbar
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        //setSupportActionBar(toolbar)
+        supportActionBar?.title = getString(R.string.MixMaker)
+
+        val mmlogo: ImageView = findViewById(R.id.mmlogo)
+
+
+        // Add to Playlist Button for Song 1
+        val addToPlaylist1 = findViewById<Button>(R.id.addToPlaylist1)
+        addToPlaylist1.setOnClickListener{
+            Toast.makeText(applicationContext, "Lover added to your playlist", Toast.LENGTH_SHORT).show()
         }
 
-        val folderButton = findViewById<ImageButton>(R.id.add)
-        folderButton.setOnClickListener{
-
+        // Add to Playlist Button for Song 2
+        val addToPlaylist2 = findViewById<Button>(R.id.addToPlaylist2)
+        addToPlaylist2.setOnClickListener{
+            Toast.makeText(applicationContext, "Perfect added to your playlist", Toast.LENGTH_SHORT).show()
         }
 
-
-        val textSearchInput = findViewById<EditText>(R.id.editTextText)
-
+        // Add to Playlist Button for Song 3
+        val addToPlaylist3 = findViewById<Button>(R.id.addToPlaylist3)
+        addToPlaylist3.setOnClickListener{
+            Toast.makeText(applicationContext, "Watermelon Sugar added to your playlist", Toast.LENGTH_SHORT).show()
+        }
     }
 }
